6f87fd080beca1537b2e4489c8b8a7ab1ff5efdd
VL <lusakvm@gmail.com>
2023-12-04 22:04:52 +0200

    proj init

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
