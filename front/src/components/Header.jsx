e893d2ab8ffa8260951f792356771977794c1fe0
VL <lusakvm@gmail.com>
2023-12-06 23:09:37 +0200

    header update

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -5,34 +5,42 @@ export const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="flex justify-between items-center bg-imp text-text p-4">
-      <img
-        src={getImageUrl("header/BROWS_LKV_LOGO.png")}
-        alt="logo"
-        className="cursor-pointer"
-      />
-      <a onClick={() => setMenuOpen(!menuOpen)} className="cursor-pointer">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          fill="#f1eee7"
-          viewBox="0 0 256 256"
+    <div>
+      <div className="flex justify-between items-center bg-imp text-text z-2">
+        <img
+          src={getImageUrl("header/Browslkv_logo-main.png")}
+          alt="logo"
+          className="cursor-pointer w-32 h-32 shadow-sm p-1 mx-3"
+        />
+        <h1 className="text-4xl">BROWS_LKV</h1>
+        <a
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="cursor-pointer mx-8"
         >
-          <path d="M222,128a6,6,0,0,1-6,6H40a6,6,0,0,1,0-12H216A6,6,0,0,1,222,128ZM40,70H216a6,6,0,0,0,0-12H40a6,6,0,0,0,0,12ZM216,186H40a6,6,0,0,0,0,12H216a6,6,0,0,0,0-12Z"></path>
-        </svg>
-      </a>
-      {menuOpen && (
-        <div>
-          <ul className="flex space-x-4 flex-col">
-            <li>Про мене</li>
-            <li>Прайс</li>
-            <li>Записатись</li>
-            <li>Галерея</li>
-            <li>Контакти</li>
-          </ul>
-        </div>
-      )}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="#f1eee7"
+            viewBox="0 0 256 256"
+          >
+            <path d="M222,128a6,6,0,0,1-6,6H40a6,6,0,0,1,0-12H216A6,6,0,0,1,222,128ZM40,70H216a6,6,0,0,0,0-12H40a6,6,0,0,0,0,12ZM216,186H40a6,6,0,0,0,0,12H216a6,6,0,0,0,0-12Z"></path>
+          </svg>
+          {menuOpen && (
+            <div>
+              <ul className="flex flex-col justify-around items-center z-1">
+                <li>
+                  <a>Про мене</a>
+                </li>
+                <li>Прайс</li>
+                <li>Записатись</li>
+                <li>Галерея</li>
+                <li>Контакти</li>
+              </ul>
+            </div>
+          )}
+        </a>
+      </div>
     </div>
   );
 };
