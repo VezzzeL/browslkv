31da5c1f4ea5f366726eeff64fea95588f363b29
VL <lusakvm@gmail.com>
2023-12-13 21:53:44 +0200

    fix for tailwind classes

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -2,19 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwind/recommended", // Add Tailwind CSS support
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "tailwind"], // Add 'tailwind' plugin
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "tailwind/class-order": "error", // Enforce Tailwind CSS class order
+    "tailwind/no-custom-classname": "error", // Disallow custom class names
   },
-}
+};
