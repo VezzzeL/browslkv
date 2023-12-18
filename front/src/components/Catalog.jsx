44ffd3f6db389e183456f736cdc9d3e83076e647
VL <lusakvm@gmail.com>
2023-12-18 21:29:46 +0200

    completerd layout

--- front/src/components/Catalog.jsx
+++ front/src/components/Catalog.jsx
@@ -6,23 +6,21 @@ export const Catalog = () => {
   return (
     <div className="text-text font-nunito">
       <div className="flex justify-center">
-        <div className="flex flex-wrap justify-center w-full max-w-screen-lg">
+        <div className="flex flex-wrap justify-center w-full">
           {catalogItems.map((item, index) => (
-            <div key={index} className="m-4">
-              <label className="block h-64">
-                <div className="rounded-xl h-full w-64 bg-secondary p-6 shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
-                  <div className="flex justify-center mb-4">
+            <div key={index} className="m-1">
+              <label className="block h-full w-full">
+                <div className="rounded-xl h-full w-64 bg-secondary p-2 shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+                  <div className="flex justify-center mb-1">
                     <img
                       src={getImageUrl(item.img)}
-                      className="rounded-full object-cover w-24 h-36"
+                      className="rounded-full object-cover w-24 h-24"
                       alt={item.text}
                     />
                   </div>
-                  <div className="flex flex-col items-center">
-                    <h2 className="text-lg font-semibold text-gray-800 mb-1">
-                      {item.price}
-                    </h2>
-                    <p className="text-sm text-gray-600">{item.text}</p>
+                  <div className="flex flex-col items-center text-center">
+                    <h2 className="text-lg font-semibold">{item.price}</h2>
+                    <p className="text-sm">{item.text}</p>
                   </div>
                 </div>
               </label>
