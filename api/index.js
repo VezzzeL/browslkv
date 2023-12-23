9c1b9d335a76b9974b805f5ae280baa95b1953ef
VL <lusakvm@gmail.com>
2023-12-23 20:59:20 +0200

    Orders get/post + models fix

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 
+const Order = require("./models/Order");
+
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
   })
 );
 
@@ -17,4 +19,45 @@ app.get("/test", (req, res) => {
   res.json("ok");
 });
 
-app.listen(4000);
+app.get("/orders", async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/orders/:id", async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    res.json(order);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/orders", async (req, res) => {
+  const order = new Order({
+    clientName: req.body.clientName,
+    phone: req.body.phone,
+    visitDate: req.body.visitDate,
+    slots: req.body.slots,
+    instagram: req.body.instagram,
+  });
+
+  try {
+    const newOrder = await order.save();
+    res.status(201).json(newOrder);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- api/models/Order.js
+++ api/models/Order.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = mongoose.Schema({
-  client: { type: String, required: true },
+  clientName: { type: String, required: true },
   phone: { type: Number, required: true },
   visitDate: { type: Date, required: true },
+  slots: [
+    {
+      time: {
+        type: String,
+        enum: ["10:00", "11:30", "13:00", "14:30", "16:00", "17:30", "19:00"],
+        required: true,
+      },
+      available: { type: Boolean, default: true },
+    },
+  ],
+  instagram: { type: String, required: false },
 });
+
+module.exports = mongoose.model("Order", OrderSchema);
